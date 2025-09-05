7df4d9d11c286b55609e082e7f191307e136adf8
Anita <jelicanita7@gmail.com>
2025-09-05 15:24:58 +0200

    Add metadata to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,14 @@ import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import localFont from "next/font/local";
 
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
+import { createClient } from "@/prismicio";
+import { isFilled } from "@prismicio/client";
 
+import { NavBar } from "@/components/NavBar";
 import { Footer } from "@/components/Footer";
 import "./globals.css";
-import { NavBar } from "@/components/NavBar";
 
 const raleway = Raleway({
   variable: "--font-raleway",
@@ -19,11 +23,31 @@ const gambarino = localFont({
   variable: "--font-gambarino",
 });
 
+export async function generateMetdata(): Promise<Metadata> {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+
+  return {
+    title: settings.data.site_title || "Côte Royale Paris",
+    description:
+      settings.data.meta_description ||
+      "Discover the exquisite collection of luxury fragrances by Côte Royale Paris",
+    openGraph: {
+      images: isFilled.image(settings.data.fallback_og_image)
+        ? [settings.data.fallback_og_image.url]
+        : ["/og-image.png"],
+    },
+  };
+}
+
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+
   return (
       <html
         lang="en"
@@ -34,6 +58,7 @@ export default async function RootLayout({
           <main className="pt-14 md:pt-16">{children}</main>
           <Footer />
         </body>
+        <PrismicPreview repositoryName={repositoryName} />
       </html>
   );
 }
