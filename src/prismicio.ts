38acdc7e6c7dadec1fb5aeda9f81038ceaac02da
Anita <jelicanita7@gmail.com>
2025-11-06 00:59:08 +0100

    Fragrance quiz

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -17,11 +17,11 @@ export const repositoryName =
  *
  * {@link https://prismic.io/docs/route-resolver#route-resolver}
  */
-// TODO: Update the routes array to match your project's route structure.
 const routes: Route[] = [
   // Examples:
   { type: "homepage", path: "/" },
   { type: "fragrance", path: "/fragrance/:uid" },
+  { type: "quiz", path: "/quiz" },
 ];
 
 /**
